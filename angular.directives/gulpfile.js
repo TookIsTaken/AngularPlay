3f0e362c07b55b0f9f90a1c50d32bdfa94e0ebeb
TookIsTaken <bmoye1986@gmail.com>
2016-01-17 16:21:54 -0500

    Added all Vendor Js Dependencies to NPM and Gulp
    All Third Party JS libraries are now handled by NPM and Gulped into a
    vendor.min.js file

--- angular.directives/gulpfile.js
+++ angular.directives/gulpfile.js
@@ -6,7 +6,17 @@ var del = require('del');
 
 var config = {
     //Include all js files but exclude any min.js files
-    src: ['app/**/*.js', '!app/**/*.min.js']
+    src: ['app/**/*.js', '!app/**/*.min.js'],
+    vendor: ['node_modules/jquery/dist/jquery.js',
+             'node_modules/angular/angular.js',
+             'node_modules/angular-animate/angular-animate.js',
+             'node_modules/angular-route/angular-route.js',
+             'node_modules/angular-sanitize/angular-sanitize.js',
+             'node_modules/bootstrap/dist/bootstrap.js',
+             'node_modules/toastr/toastr.js',
+             'node_modules/moment/moment.js',
+             'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
+             'node_modules/spin/dist/spin.js']
 }
 
 //delete the output file(s)
@@ -27,6 +37,13 @@ gulp.task('scripts', ['clean'], function () {
       .pipe(concat('all.min.js'))
       .pipe(gulp.dest('app/'));
 });
+gulp.task('vendor', ['clean'], function () {
+
+    return gulp.src(config.vendor)
+      .pipe(uglify())
+      .pipe(concat('vendor.min.js'))
+      .pipe(gulp.dest('app/'));
+});
 
 //Set a default tasks
-gulp.task('default', ['scripts'], function () { });
+gulp.task('default', ['scripts', 'vendor'], function () { });
