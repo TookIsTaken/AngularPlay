3c5fe038b462ca124681508336535c396f7536f7
TookIsTaken <bmoye1986@gmail.com>
2016-01-18 14:44:32 -0500

    Created Services for Triplet Calculations and Rounding
    Moved calculation logic and rounding logic into services

---
+++ angular.directives/app/services/TripletCalculations.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'tripletCalculations';
+    angular.module('app').factory(serviceId, ['rounding', tripletCalculations]);
+
+    function tripletCalculations(roundingServ) {
+       
+        var service = {
+            calculateUnitCost: calcUc,
+            calculateTotalCost: calcTc
+        };
+
+        return service;
+
+        function calcUc(qty, tc, currentTcUom, neededUom, neededPrecision) {
+            var uc = roundingServ.unRound(tc, currentTcUom) / qty;
+            return roundingServ.round(uc, neededUom, neededPrecision);
+        }
+
+        function calcTc(qty, uc, currentUcUom, neededUom, neededPrecision) {
+            var tc = roundingServ.unRound(uc, currentucUom) * qty;
+            return roundingServ.round(tc, neededUom, neededPrecision);
+        }
+    }
+})();
---
+++ angular.directives/app/services/rounding.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'rounding';
+    angular.module('app').factory(serviceId, [rounding]);
+
+    function rounding() {
+
+        var service = {
+            round: roundValue,
+            unRound: unRoundValue
+        };
+
+        return service;
+        
+        function round(value, decimals) {
+            return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+        }
+
+        //Returns the value in Eaches/Dollars/etc.
+        function unRoundValue(val, currentUom) {
+            if (currentUom == 1) {
+                return val;
+            } else if (currentUom == 2) {
+                return val * 1000;
+            } else if (currentUom == 3) {
+                return val * 10000;
+            }
+        }
+
+        //Takes in an unRounded value and returns in correct UOM
+        function roundValue(val, neededUom, precision) {
+            var roundBase = 1; //Eaches/Dollars
+            if (neededUom == 2) { //Thousands
+                roundBase = 1000;
+            } else if (neededUom == 3) { //Millions
+                roundBase = 10000;
+            }
+
+            return addTrailingZeros(round(val / roundBase, precision), precision);
+        }
+
+        //Rounding leaves off trailing zeros, this add them back
+        function addTrailingZeros(val, precision) {
+            return val.toFixed(precision);
+        }
+    }
+})();
